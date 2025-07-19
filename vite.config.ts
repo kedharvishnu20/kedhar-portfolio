f911e5e709c7017662ca72912500d0a4f5fba106
kedhar vishnu buddepu <kedharvishnu1926@gmail.com>
2025-07-19 23:16:09 +0530

    last

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/kedhar-portfolio/',
   server: {
     host: "::",
     port: 8080,
