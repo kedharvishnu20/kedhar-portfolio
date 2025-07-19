6b43b60a6bf3477f9a4dee006b3eba1e6ef1aeae
kedhar vishnu buddepu <112081902+kedharvishnu20@users.noreply.github.com>
2025-07-20 00:00:54 +0530

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -47,7 +47,7 @@ const Navigation = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex-shrink-0">
-            <span className="text-xl font-bold gradient-text">Kedhar Vishnu Buddepu</span>
+            <span className="text-xl font-bold gradient-text">My Portfolio</span>
           </div>
 
           {/* Desktop Navigation */}
